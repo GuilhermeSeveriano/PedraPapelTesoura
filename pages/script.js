34be654b08662782b64e9d5d0ed13a6da9ceeb85
GuilhermeSeveriano <guiseveriano.10@gmail.com>
2024-05-11 17:51:43 -0300

    Projeto pronto

---
+++ pages/script.js
@@ -0,0 +1,94 @@
+var elementos = document.querySelectorAll(".player div > img");
+var selectOpt = "";
+var inimigoOpt = "";
+
+function validarVitoria() {
+  
+
+  let vencedor = document.querySelector(".resultado");
+
+  if (selectOpt == "Papel") {
+    if (inimigoOpt == "Papel") {
+      vencedor.innerHTML= "O Jogo Empatou";
+      vencedor.style.color = "blue";    
+
+    } else if (inimigoOpt == "Pedra") {
+      vencedor.innerHTML = "O Player Venceu";
+      vencedor.style.color = "green";
+
+    } else if (inimigoOpt == "Tesoura") {
+      vencedor.innerHTML = "O Adversário Venceu";
+      vencedor.style.color = "red";
+
+    }
+
+  }
+
+  if (selectOpt == "Pedra") {
+    if (inimigoOpt == "Papel") {
+      vencedor.innerHTML = "O Adversário Venceu";
+      vencedor.style.color = "red";
+
+    } else if (inimigoOpt == "Pedra") {
+      vencedor.innerHTML = "O Jogo Empatou";
+      vencedor.style.color = "blue";
+
+    } else if (inimigoOpt == "Tesoura") {
+      vencedor.innerHTML = "O Player Venceu";
+      vencedor.style.color = "green";
+    }
+  }
+
+  if (selectOpt == "Tesoura") {
+    if (inimigoOpt == "Papel") {
+      vencedor.innerHTML = "O Player Venceu";
+      vencedor.style.color = "green";
+
+    } else if (inimigoOpt == "Pedra") {
+      vencedor.innerHTML = "O Adversário Venceu";
+      vencedor.style.color = "red";
+
+    } else if (inimigoOpt == "Tesoura") {
+      vencedor.innerHTML = "O Jogo Empatou";
+      vencedor.style.color = "blue";
+    }
+  }
+}
+
+function resetInimigo() {
+  const enemyOptions = document.querySelectorAll(".inimigo div > img");
+  for (var i = 0; i < enemyOptions.length; i++) {
+    enemyOptions[i].style.opacity = 0.5;
+  }
+}
+
+function inimigoJogar() {
+  let rand = Math.floor(Math.random() * 3);
+  const enemyOptions = document.querySelectorAll(".inimigo div > img");
+  resetInimigo();
+
+  for (var i = 0; i < enemyOptions.length; i++) {
+    if (i == rand) {
+      enemyOptions[i].style.opacity = 1;
+      inimigoOpt = enemyOptions[i].getAttribute("opt");
+    }
+  }
+
+  validarVitoria();
+}
+
+function resetOpacity() {
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].style.opacity = 0.5;
+  }
+}
+
+for (var i = 0; i < elementos.length; i++) {
+  elementos[i].addEventListener("click", function (t) {
+    resetOpacity();
+    t.target.style.opacity = 1;
+    selectOpt = t.target.getAttribute("opt");
+
+    inimigoJogar();
+  });
+}
